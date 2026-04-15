dd98d2e27508a58a2428878babc3fb99c2c03e34
thejarrus <contact@novamentstudios.com>
2026-04-16 00:31:24 +0300

    🐍 Gün 4: String işlemleri ve slicing

---
+++ gun04-string-islemleri/main.py
@@ -0,0 +1,14 @@
+# 30 Günde Python - Gün 4: String İşlemleri
+mesaj = "Merhaba"
+print(mesaj[0])
+print(mesaj[1])
+print(len(mesaj))
+print(mesaj[0:3])
+print(mesaj[3:])
+print(mesaj[:4])
+
+mesaj = "Merhaba Dünya"
+print(mesaj.upper())
+print(mesaj.lower())
+print(mesaj.replace("Dünya", "Python"))
+print(mesaj.split())
