7275cf3b8b96bbf32e32460c1770af862b8a8c11
thejarrus <contact@novamentstudios.com>
2026-04-16 00:31:06 +0300

    🐍 Gün 2: Değişkenler ve veri tipleri

---
+++ gun02-degiskenler/main.py
@@ -0,0 +1,17 @@
+# 30 Günde Python - Gün 2: Değişkenler
+isim = "Yiğit"
+yas = 20
+boy = 1.78
+ogrenci = True
+
+print(type(isim))
+print(type(yas))
+print(type(boy))
+print(type(ogrenci))
+
+sayac = 0
+print(sayac)
+sayac += 5
+print(sayac)
+sayac += 5
+print(sayac)
